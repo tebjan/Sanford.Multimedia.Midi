bc73a0b54b1c7161def1ac61d16fcb4acbc0afc5
tebjan <tebjan@vvvv.org>
2015-11-25 19:39:23 +0100

    upped version to 6.0.0.0 and removed some references

--- Midi/Properties/AssemblyInfo.cs
+++ Midi/Properties/AssemblyInfo.cs
@@ -27,7 +27,7 @@ using System.Runtime.CompilerServices;
 // You can specify all the values or you can default the Revision and Build Numbers 
 // by using the '*' as shown below:
 
-[assembly: AssemblyVersion("5.1.0.0")]
+[assembly: AssemblyVersion("6.0.0.0")]
 
 //
 // In order to sign your assembly you must specify a key to use. Refer to the 
--- Midi/Sanford.Multimedia.Midi/UI/InputDeviceDialog.cs
+++ Midi/Sanford.Multimedia.Midi/UI/InputDeviceDialog.cs
@@ -35,7 +35,6 @@
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.Data;
 using System.Drawing;
 using System.Windows.Forms;
 
