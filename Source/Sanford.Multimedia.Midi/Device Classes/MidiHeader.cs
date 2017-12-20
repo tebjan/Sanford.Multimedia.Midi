610721b0d845dbc22d043e807dd51a16ebf86b12
DKing <DKingCN@users.noreply.github.com>
2017-12-20 17:53:58 +0800

    Fix MIDIHDR implement in x64
    Some element types are actually not Int32(DWORD 32b) but IntPtr(DWORD_PTR 64b). Fixed.
    Tho they may be not affecting because these elements are not used, but anyway, they are standard MIDIHDR now.
    
    (cherry picked from commit 593fd0c27e19210b7cba7fb7530bf8e7c8b648a3)

--- Source/Sanford.Multimedia.Midi/Device Classes/MidiHeader.cs
+++ Source/Sanford.Multimedia.Midi/Device Classes/MidiHeader.cs
@@ -40,7 +40,7 @@ namespace Sanford.Multimedia.Midi
     /// <summary>
     /// Represents the Windows Multimedia MIDIHDR structure.
     /// </summary>
-    [StructLayout(LayoutKind.Sequential)]
+    [StructLayout(LayoutKind.Sequential, Pack = 1)]
     internal struct MidiHeader
     {
         #region MidiHeader Members
@@ -53,23 +53,23 @@ namespace Sanford.Multimedia.Midi
         /// <summary>
         /// Size of the buffer.
         /// </summary>
-        public int bufferLength; 
+        public Int32 bufferLength; 
 
         /// <summary>
         /// Actual amount of data in the buffer. This value should be less than 
         /// or equal to the value given in the dwBufferLength member.
         /// </summary>
-        public int bytesRecorded; 
+        public Int32 bytesRecorded; 
 
         /// <summary>
         /// Custom user data.
         /// </summary>
-        public int user; 
+        public IntPtr user; 
 
         /// <summary>
         /// Flags giving information about the buffer.
         /// </summary>
-        public int flags; 
+        public Int32 flags; 
 
         /// <summary>
         /// Reserved; do not use.
@@ -79,7 +79,7 @@ namespace Sanford.Multimedia.Midi
         /// <summary>
         /// Reserved; do not use.
         /// </summary>
-        public int reserved; 
+        public IntPtr reserved; 
 
         /// <summary>
         /// Offset into the buffer when a callback is performed. (This 
@@ -88,13 +88,13 @@ namespace Sanford.Multimedia.Midi
         /// This offset enables an application to determine which 
         /// event caused the callback. 
         /// </summary>
-        public int offset; 
+        public Int32 offset; 
 
         /// <summary>
         /// Reserved; do not use.
         /// </summary>
-        [MarshalAs(UnmanagedType.ByValArray, SizeConst=8)]
-        public int[] reservedArray; 
+        [MarshalAs(UnmanagedType.ByValArray, SizeConst = 8)]
+        public IntPtr[] reservedArray; 
 
         #endregion
     }
